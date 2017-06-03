894d5932f3952c743cad83897594c37aefaed927
Ashish Gupta <developerashishgupta@gmail.com>
2017-06-04 01:09:30 +0530

    made view request forn

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {ServiceRequestListComponent} from '../service-request-list/service-request-list.component';
+import {ViewServiceRequestComponent} from '../view-service-request/view-service-request.component';
 @Component({
     selector:'dashboard',
     templateUrl:'./dashboard.component.html'
---
+++ src/assets/materilaize-setup.js
@@ -0,0 +1,7 @@
+$(function() {
+  // Handler for .ready() called.
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+});
